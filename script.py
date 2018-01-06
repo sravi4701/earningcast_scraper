9a81205289225feaeb84b2e162f35dfb39eb3c8b
Ravi Shankar <sravi4701@gmail.com>
2018-01-06 19:20:33 +0530

    update: update script.py

--- script.py
+++ script.py
@@ -6,13 +6,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import threading
+import os
+
+
 # ajax call url
 # https://earningscast.com//calls?ajax=true&ajax_render=latest&page=1
 
+# global variabls
 
 head_url = "https://earningscast.com"
 store = {}
-
 '''
 Store structure
 store {
@@ -38,6 +41,33 @@ def fetch_audio_url(item_url):
 
 def log(message):
 	print(message, file=sys.stderr)
+
+
+def save_to_file(*data):
+	audio_url = data[-1]
+	company_name = data[0]
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	os.chdir(dir_path)
+	unique_hash = audio_url.split("/")[-2]
+	dir_name = company_name + "/event-" + unique_hash 
+	os.makedirs(dir_name)
+	os.chdir(dir_name)
+	with open("data-" + unique_hash + ".txt", "w") as f:
+		f.write("name:" + data[1])
+		f.write("\ndate:" + data[2])
+		f.write("\n" + data[3] + "\n" + data[4])
+		f.close()
+	session = requests.Session()
+	payload = {'user[login]': 'useremail', 'user[password]': 'userpass'}
+	r = session.post('https://earningscast.com/users/sign_in', data=payload)
+	log("logged in....")
+	with open("data-" + unique_hash + ".m4a", "wb") as fhand:
+		response = session.get(audio_url, stream=True)
+		for block in response.iter_content(1024):
+			fhand.write(block)
+		fhand.close()
+	log("done downloading....")
+	os.chdir(dir_path)
 
 def parse_source(soup):
 	'''
@@ -69,26 +99,27 @@ def parse_source(soup):
 			est = est.strip()
 		log("fetching audio url ..........")
 		audio_url = head_url + fetch_audio_url(item_url)
-		if not store.get(company_name):
-			store[company_name] = []
-		obj = {
-			"event_name":name,
-			"event_date": date,
-			"Act":act,
-			"Est":est,
-			"audio_download_link":audio_url
-			}
-		store[company_name].append(obj)
-		# log("company_name:" + company_name)
-		# log("Name:" +  str(name))
-		# log("date:" +  str(date))
-		# log("Act:" + str(act))
-		# log("Est:" +  str(est))
-		# log("audio_url:" + str(audio_url))
+		# if not store.get(company_name):
+		# 	store[company_name] = []
+		# obj = {
+		# 	"event_name":name,
+		# 	"event_date": date,
+		# 	"Act":act,
+		# 	"Est":est,
+		# 	"audio_download_link":audio_url
+		# 	}
+		# store[company_name].append(obj)
+		save_to_file(company_name, name, date, act, est, audio_url)
+		log("company_name:" + company_name)
+		log("Name:" +  str(name))
+		log("date:" +  str(date))
+		log("Act:" + str(act))
+		log("Est:" +  str(est))
+		log("audio_url:" + str(audio_url))
 		log("--"*10)
 
 def main():
-	for i in range(1, 5):
+	for i in range(1, 1000):
 		log("page" + str(i) + " ........")
 		url = head_url + "/calls?ajax=true&ajax_render=latest&page=" + str(i)
 		response = requests.get(url)
@@ -98,7 +129,7 @@ def main():
 		t.start()
 	while threading.active_count() > 1:
 		continue
-	print(store)
 
 if __name__ == '__main__':
+	os.chdir(".")
 	main()
